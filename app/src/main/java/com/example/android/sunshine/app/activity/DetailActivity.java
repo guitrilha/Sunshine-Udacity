886f87c3beea2f80e00209db0dcc6bf2f2425af0
guitrilha <gui_5c@hotmail.com>
2016-08-03 12:59:58 -0300

    Acessando o mapa da localizacao e tambem compartilhando a previsao nos details

--- app/src/main/java/com/example/android/sunshine/app/activity/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/activity/DetailActivity.java
@@ -2,7 +2,9 @@ package com.example.android.sunshine.app.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -11,14 +13,17 @@ import com.example.android.sunshine.app.R;
 
 public class DetailActivity extends ActionBarActivity {
 
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private String mForecast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         Intent intent = getIntent();
         if (intent != null) {
-            String forecast = intent.getStringExtra(Intent.EXTRA_TEXT);
-            ((TextView) findViewById(R.id.forecast_textview)).setText(forecast);
+            mForecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) findViewById(R.id.forecast_textview)).setText(mForecast);
         }
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
@@ -26,7 +31,12 @@ public class DetailActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.detail, menu);
+
+        //Share detail msg
+        MenuItem shareItem = menu.findItem(R.id.action_share);
+        ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        shareActionProvider.setShareIntent(createShareForecastIntent());
         return true;
     }
 
@@ -43,5 +53,13 @@ public class DetailActivity extends ActionBarActivity {
     private void onSettingsClicked() {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
+    }
+
+    private Intent createShareForecastIntent(){
+        Intent myShareIntent = new Intent(Intent.ACTION_SEND);
+        myShareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        myShareIntent.setType("text/plain");
+        myShareIntent.putExtra(Intent.EXTRA_TEXT, mForecast + FORECAST_SHARE_HASHTAG);
+        return myShareIntent;
     }
 }
--- app/src/main/java/com/example/android/sunshine/app/activity/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/activity/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.android.sunshine.app.activity;
 
-import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -37,8 +40,24 @@ public class MainActivity extends ActionBarActivity {
             case R.id.action_settings:
                 this.onSettingsClicked();
                 break;
+            case R.id.action_map:
+                this.onPreferredLocationInMap();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void onPreferredLocationInMap() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String postalCode = preferences.getString(getString(R.string.settings_local_key), getString(R.string.settings_local_defaultValue));
+        Uri parse = Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q", postalCode + ",BR").build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(parse);
+        if((intent.resolveActivity(getPackageManager()) != null)){
+            startActivity(intent);
+        }else{
+            Log.d("MainActivity","Não foi possível encontrar um aplicativo de mapas no seu dispositivo.");
+        }
+
     }
 
     private void onSettingsClicked() {
